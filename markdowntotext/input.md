54c4bd2bc51a42820bff85a2557bdc2cfdba7635
mfuenzalida1 <mfuenzalida1@uc.cl>
2020-03-26 13:45:00 -0300

    markdown to text finished

--- markdowntotext/input.md
+++ markdowntotext/input.md
@@ -7,22 +7,27 @@ Las posibles consecuencias de ello son
 1. Primera
 2. Segunda
 3. Tercera
+
 En cambio no es necesario considerar cosas como
 * algo
 * algo mas
 * etcetera
 ## Segunda parte
 Ahora otra sección para mostrar como mostrar código
+
+`hola`
+```
 def say_goodnight(name)
-result = "Good night, #{name}"
-return result
+    result = "Good night, #{name}"
+    return result
 end
 #puts say_goodnight "Jaime"
 class BookInStock
-def initialize(isbn, price)
-@isbn = isbn
-@price = Float(price)
+    def initialize(isbn, price)
+        @isbn = isbn
+        @price = Float(price)
+    end
 end
-end 
+```
 Insertar una línea es muy fácil
 ***
--- markdowntotext/main.js
+++ markdowntotext/main.js
@@ -8,9 +8,16 @@ const lines = readline.createInterface({
     input: fs.createReadStream(input)
 });
 
+// Lists memory values
 let currentOrderedValue = -1;
 let currentUnorderedValue = -1;
+
+// Line breaks boolean
 let shouldLineBreak = false;
+
+// Code block and identation value
+let shouldTab = 0;
+let openCodeBlock = false;
 
 const compose = (a, b) => (c) => a(b(c));
 
@@ -24,19 +31,116 @@ const fetchHeaderSize = line => {
     return count;
 };
 
+// Checks if line starts with numbers
+const startsWithDigit = line => {
+    line = line.trim();
+    if (line.substring(0, 1).match(/^\d+$/)) {
+        return true;
+    }
+    return false;
+};
+
+// Checks if line starts with bullet * and space
+const startsWithBullet = line => {
+    line = line.trim();
+    const symbols = ['*', '-', '+'];
+    for (const symbol of symbols) {
+        if (line.charAt(0) === symbol && line.charAt(1) === ' ') {
+            return true;
+        }
+    }
+    return false;
+};
+
 // Detects if line is a code block start/end (```, ```python)
 const isLimitCodeBlock = line => {
     let count = (line.match(/```/g) || []).length;
     return count === 1;
 };
 
+// Checks if line has a block starter
+const hasBlockStarter = line => {
+    line = line.trim();
+    const symbols = ['def', 'class', 'if', "for"];
+    for (const symbol of symbols) {
+        if (line.substring(0, 5).includes(symbol)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// Checks if line has block ender
+const hasBlockEnder = line => {
+    line = line.trim();
+    const endBlock = ['end'];
+    for (const symbol of endBlock) {
+        if (line.substring(0, 6).includes(symbol)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+// Parse code line inside block to indented text
+const codeLineToText = line => {
+    if (isLimitCodeBlock(line)) {
+        let type = codeBlockType(line);
+        if (type !== "none") {
+            line = line.replace(type, '');
+        }
+        line = line.replace('```', '');
+    } else {
+        if (openCodeBlock) {
+            // Check identation inside block
+            line = line.trim();
+            const nextTab = hasBlockStarter(line);
+            const nextBack = hasBlockEnder(line);
+            if (nextBack) {
+                shouldTab--;
+            }
+            if (shouldTab > 0) {
+                const tabs = '\t'.repeat(shouldTab);
+                line = tabs + line;
+            }
+            if (nextTab) {
+                shouldTab++;
+            }
+        }
+    }
+    return line;
+};
+
+// Detect code block type
+const codeBlockType = line => {
+    let type = line.substring(3);
+    if (type === '') {
+        type = "none";
+    }
+    openCodeBlock = !openCodeBlock;
+    return type;
+};
+
+// Parse code highlights to text (`hello` -> hello)
+const codeHighlightToText = line => {
+    if (!isLimitCodeBlock(line)) {
+        line = line.trim();
+        let text = line.replace(/`/g, '');
+        return text;
+    }
+    return line;
+};
+
 // Converts headers to text format (## Hello -> Hello)
 const headersToText = line => {
-    line = line.trim();
-    const count = fetchHeaderSize(line);
-    let text = line.substring(count, line.length).trim();
-    if (count > 0) {
-        text = text + "\n";
+    let text = line;
+    if (!openCodeBlock) {
+        line = line.trim();
+        const count = fetchHeaderSize(line);
+        text = line.substring(count, line.length).trim();
+        if (count > 0) {
+            text = text + "\n";
+        }
     }
     return text;
 };
@@ -49,28 +153,9 @@ const horizontalRuleToText = line => {
     return line;
 };
 
-const startsWithDigit = line => {
-    line = line.trim();
-    if (line.substring(0, 1).match(/^\d+$/)) {
-        return true;
-    }
-    return false;
-};
-
-const startsWithBullet = line => {
-    line = line.trim();
-    const symbols = ['*', '-', '+'];
-    for (const symbol of symbols) {
-        if (line.charAt(0) === symbol && line.charAt(1) === ' ') {
-            return true;
-        }
-    }
-    return false;
-};
-
 // Converts unordered list to text format (* Hello -> \t Hello)
 const unorderedListToText = line => {
-    if (startsWithBullet(line)) {
+    if (startsWithBullet(line) && !openCodeBlock) {
         let start = "";
         if (currentUnorderedValue === -1) {
             currentUnorderedValue = 1;
@@ -92,7 +177,7 @@ const unorderedListToText = line => {
 // Converts ordered list to text format (1. Hello -> \t 1. Hello)
 const orderedListToText = line => {
     // line = line.trim();
-    if (startsWithDigit(line) && (line.match(/./g) || []).length > 0) {
+    if (startsWithDigit(line) && (line.match(/./g) || []).length > 0 && !openCodeBlock) {
         parts = line.split(".");
         if ((parts[0].match(/^\d+$/) || []).length > 0 && parts[1].charAt(0) === ' ') {
             let start = "";
@@ -116,7 +201,7 @@ const orderedListToText = line => {
 };
 
 // pipeline to set all parsing '*ToHtml' functions
-const parserPipeline = pipe([headersToText, horizontalRuleToText, unorderedListToText, orderedListToText]);
+const parserPipeline = pipe([codeHighlightToText, codeLineToText, headersToText, horizontalRuleToText, unorderedListToText, orderedListToText]);
 
 // run pipeline on lines that are not empty and returns the parsed line
 const parseLine = line => {
